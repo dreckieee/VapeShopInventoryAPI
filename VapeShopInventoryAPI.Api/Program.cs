65acac42e429371ca259e9d890fa1e5d06f1577b
dreckieee <dreckrjpascual@gmail.com>
2026-07-03 18:47:19 +0800

    feat: wire controllers and DbContext in Program.cs

--- VapeShopInventoryAPI.Api/Program.cs
+++ VapeShopInventoryAPI.Api/Program.cs
@@ -1,6 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<VapeShopInventoryDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("VapeShopInventory")));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapControllers();
 
 app.Run();
