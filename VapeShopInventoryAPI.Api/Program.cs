c0739c7c7d9a500e8cdab0ef334d1427d9f091ff
dreckieee <dreckrjpascual@gmail.com>
2026-07-04 17:28:31 +0800

    feat: add Swagger UI for API documentation

--- VapeShopInventoryAPI.Api/Program.cs
+++ VapeShopInventoryAPI.Api/Program.cs
@@ -6,7 +6,16 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<VapeShopInventoryDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("VapeShopInventory")));
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.MapControllers();
 
