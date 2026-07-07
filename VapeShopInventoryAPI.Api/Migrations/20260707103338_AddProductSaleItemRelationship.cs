0f282bd20fcfe438a66f347e26957733e2e14ab7
dreckieee <dreckrjpascual@gmail.com>
2026-07-07 18:42:17 +0800

    feat: add Product foreign key relationship to SaleItem

---
+++ VapeShopInventoryAPI.Api/Migrations/20260707103338_AddProductSaleItemRelationship.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VapeShopInventoryAPI.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductSaleItemRelationship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_SaleItems_ProductId",
+                table: "SaleItems",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SaleItems_Products_ProductId",
+                table: "SaleItems",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SaleItems_Products_ProductId",
+                table: "SaleItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SaleItems_ProductId",
+                table: "SaleItems");
+        }
+    }
+}
