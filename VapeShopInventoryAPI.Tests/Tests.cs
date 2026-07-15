0724ecf93ba47f4b349bd6d2d47cefea7fdb9a2c
dreckieee <dreckrjpascual@gmail.com>
2026-07-15 21:06:29 +0800

    feat: scaffold Playwright NUnit test project

---
+++ VapeShopInventoryAPI.Tests/Tests.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.Playwright.NUnit;
+
+namespace VapeShopInventoryAPI.Tests;
+
+public class Tests : PageTest
+{
+    [Test]
+    public async Task Test1()
+    {
+        await Page.GotoAsync(url: "https://playwright.dev");
+        await Expect(Page).ToHaveTitleAsync(new Regex("Playwright"));
+    }
+}
