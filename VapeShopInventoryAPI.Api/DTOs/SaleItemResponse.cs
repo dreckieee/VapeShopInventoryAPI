691f0a5c10c69c8c39715a6981ab43fa1e4b1475
dreckieee <dreckrjpascual@gmail.com>
2026-07-23 13:02:52 +0800

    refactor: convert SaleItemResponse to record with init properties

--- VapeShopInventoryAPI.Api/DTOs/SaleItemResponse.cs
+++ VapeShopInventoryAPI.Api/DTOs/SaleItemResponse.cs
@@ -1,8 +1,16 @@
-public class SaleItemResponse
+public record SaleItemResponse
 {
-    public int Id {get; set;}
-    public int ProductId {get; set;}
-    public int Quantity {get; set;}
-    public decimal UnitPriceAtSale {get; set;}
-    public int TransactionNumber {get; set;}
+    public int Id {get; init;}
+    public int ProductId {get; init;}
+    public int Quantity {get; init;}
+    public decimal UnitPriceAtSale {get; init;}
+    public int TransactionNumber {get; init;}
+    public static SaleItemResponse FromSaleItem (SaleItem saleItem) => new()
+    {
+        Id = saleItem.Id,
+        ProductId = saleItem.ProductId,
+        Quantity = saleItem.Quantity,
+        UnitPriceAtSale = saleItem.UnitPriceAtSale,
+        TransactionNumber = saleItem.TransactionNumber
+    };
 }
