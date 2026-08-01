2a4d81d256ff0b82532ceaaab5775ab59c3cc061
dreckieee <dreckrjpascual@gmail.com>
2026-08-01 23:04:47 +0800

    feat: migration for CreatedAt column with Date backfill

---
+++ VapeShopInventoryAPI.Api/Migrations/20260801100048_AddCreatedAtToExpense.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VapeShopInventoryAPI.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCreatedAtToExpense : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Expenses",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+                migrationBuilder.Sql("UPDATE Expenses SET CreatedAt = Date");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Expenses");
+        }
+    }
+}
