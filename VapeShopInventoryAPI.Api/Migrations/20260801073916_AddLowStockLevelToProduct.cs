4d5a394e7f842f307af18e6c0bc124ee4532cb3c
dreckieee <dreckrjpascual@gmail.com>
2026-08-01 23:01:38 +0800

    feat: migration for LowStockLevel column

---
+++ VapeShopInventoryAPI.Api/Migrations/20260801073916_AddLowStockLevelToProduct.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VapeShopInventoryAPI.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLowStockLevelToProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "LowStockLevel",
+                table: "Products",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LowStockLevel",
+                table: "Products");
+        }
+    }
+}
