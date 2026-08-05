30d32871a1078d5a9f8404183f3631ba5eb8c110
dreckieee <dreckrjpascual@gmail.com>
2026-08-05 10:24:48 +0800

    refactor: remove old RestockItem DTO file, superseded by RestockItemRequest

--- VapeShopInventoryAPI.Api/DTOs/RestockItem.cs
+++
@@ -1,7 +0,0 @@
-namespace VapeShopInventoryAPI.Api.DTOs;
-public class RestockItem
-{
-    public int ProductId {get; set;}
-    public int Quantity {get; set;}
-    public decimal UnitCost {get; set;}
-}
