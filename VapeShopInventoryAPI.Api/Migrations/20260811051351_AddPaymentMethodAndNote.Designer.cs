f7f59e4f781f41e98f9cfa8613cb7c6b171a4847
dreckieee <dreckrjpascual@gmail.com>
2026-08-11 17:37:47 +0800

    feat: add PaymentMethod/PaymentNote migration for Sales and Expenses

--- VapeShopInventoryAPI.Api/Migrations/20260811051351_AddPaymentMethodAndNote.Designer.cs
+++ VapeShopInventoryAPI.Api/Migrations/20260811051351_AddPaymentMethodAndNote.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VapeShopInventoryAPI.Api;
 
@@ -10,9 +11,11 @@ using VapeShopInventoryAPI.Api;
 namespace VapeShopInventoryAPI.Api.Migrations
 {
     [DbContext(typeof(VapeShopInventoryDbContext))]
-    partial class VapeShopInventoryDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260811051351_AddPaymentMethodAndNote")]
+    partial class AddPaymentMethodAndNote
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.10");
---
+++ VapeShopInventoryAPI.Api/Migrations/20260811051351_AddPaymentMethodAndNote.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VapeShopInventoryAPI.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPaymentMethodAndNote : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PaymentMethod",
+                table: "Sales",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentNote",
+                table: "Sales",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PaymentMethod",
+                table: "Expenses",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentNote",
+                table: "Expenses",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentMethod",
+                table: "Sales");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentNote",
+                table: "Sales");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentMethod",
+                table: "Expenses");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentNote",
+                table: "Expenses");
+        }
+    }
+}
