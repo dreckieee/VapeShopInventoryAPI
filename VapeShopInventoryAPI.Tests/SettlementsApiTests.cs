955fbcdbdcdeeda12e9b16c2ace6b885e1c7164b
dreckieee <dreckrjpascual@gmail.com>
2026-09-13 00:51:44 +0800

    test: add UpdateExpense_DateEqualsEarliestSettlement_ReturnsOk

--- VapeShopInventoryAPI.Tests/SettlementsApiTests.cs
+++ VapeShopInventoryAPI.Tests/SettlementsApiTests.cs
@@ -451,6 +451,49 @@ public class SettlementsApiTests
     }
 
     [Test]
+    public async Task UpdateExpense_DateEqualsEarliestSettlement_ReturnsOk()
+    {
+        var (_, testExpense) = await CreateTestExpenseAsync(paymentMethod: PaymentMethod.Payable, date: new DateTime(2026, 01, 01));
+
+        var payloadSettlement = new CreateSettlementRequest
+        {
+            SaleId = null,
+            ExpenseId = testExpense.Id,
+            Amount = testExpense.Amount / 2,
+            PaymentMethod = PaymentMethod.Cash,
+            PaymentNote = "Test payment note for create expense test (valid) with existing settlement update expense date equals settlement date",
+            Date = new DateTime(2026, 01, 02)
+        };
+
+        var responsePartialSettlement = await _client.PostAsJsonAsync("api/Settlements", payloadSettlement);
+        Assert.That(responsePartialSettlement.StatusCode, Is.EqualTo(HttpStatusCode.Created), $"Expected 201 Created() status, but received {responsePartialSettlement.StatusCode} instead.");
+        
+        var payload = new UpdateExpenseRequest
+        {
+            PaymentMethod = testExpense.PaymentMethod,
+            PaymentNote = "test payment note for update expense (valid) date equals earliest settlement date",
+            Description = "test description for update expense (valid) date equals earliest settlement date",
+            Amount = testExpense.Amount,
+            Category = "test category",
+            Date = payloadSettlement.Date
+        };
+
+        var response = await _client.PutAsJsonAsync($"api/Expenses/{testExpense.Id}", payload);
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK), $"Expected 200 Ok() status, but received {response.StatusCode} instead.");
+
+        var expense = await response.Content.ReadFromJsonAsync<ExpenseResponse>(TestJsonOptions.Default);
+        Assert.That(expense, Is.Not.Null);
+        Assert.That(expense.Id, Is.EqualTo(testExpense.Id));
+        Assert.That(expense.PaymentMethod, Is.EqualTo(payload.PaymentMethod));
+        Assert.That(expense.PaymentNote, Is.EqualTo(payload.PaymentNote));
+        Assert.That(expense.Description, Is.EqualTo(payload.Description));
+        Assert.That(expense.Amount, Is.EqualTo(payload.Amount));
+        Assert.That(expense.Category, Is.EqualTo(payload.Category));
+        Assert.That(expense.Date, Is.EqualTo(payload.Date));
+        Assert.That(expense.CreatedAt, Is.EqualTo(testExpense.CreatedAt));
+    }
+
+    [Test]
     public async Task UpdateExpense_DateAfterEarliestSettlement_ReturnsConflict()
     {
         var (_, testExpense) = await CreateTestExpenseAsync(paymentMethod: PaymentMethod.Payable, date: new DateTime(2026, 01, 01));
